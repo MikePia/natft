de45ab1359a45da07239ecafc5c06aa29198ffdf
MikePia <lynnpete@duck.com>
2023-09-28 21:06:17 -0600

    Added gateway. route tabe and route resources

--- modules/vpc/main_vpc.tf
+++ modules/vpc/main_vpc.tf
@@ -13,12 +13,39 @@ resource "aws_vpc" "mtc_vpc" {
 
 
 resource "aws_subnet" "mtc_public_subnet" {
-  vpc_id = aws_vpc.mtc_vpc.id
-  cidr_block = "10.123.1.0/24"
+  vpc_id                  = aws_vpc.mtc_vpc.id
+  cidr_block              = "10.123.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
+  availability_zone       = "us-east-1a"
 
   tags = {
     Name = "dev-public"
   }
+}
+
+
+resource "aws_internet_gateway" "mtc_internet_gateway" {
+  vpc_id = aws_vpc.mtc_vpc.id
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
+resource "aws_route_table" "mtc_public_rt" {
+  vpc_id = aws_vpc.mtc_vpc.id
+
+
+  tags = {
+    Name = "dev-route-table"
+  }
+}
+
+
+resource "aws_route" "default_route" {
+
+  route_table_id            = aws_route_table.mtc_public_rt.id
+
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.mtc_internet_gateway.id
+
 }
